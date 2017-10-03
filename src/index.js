8eaec4455f3cdbadda9fc5d1b636426bca31ca50
Michael <mmonh@uci.edu>
2017-10-02 20:59:11 -0700

    hello world

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+//create first component
+const App = () => {
+    
+    return (
+        <div>
+         <h1> Hello World! </h1>
+         <p> I love saying hello world</p>
+        </div>
+)
+}
+
+//render the component
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
